bc73ab6a9a51eee710fd267b006eecb369fec529
johntrooney <100171856+johntrooney@users.noreply.github.com>
2022-02-21 18:59:37 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+// kelvin is always 293
+
+const kelvin = 293;
+
+// celcius is always 293-273
+
+var celcius = (kelvin - 273);
+
+// fahrenheit fron celcius conversion, rounded down
+
+var fahrenheit = celcius * ((9/5) + 32));
+
+var TEMPERATURE = fahrenheit;
+
+// console.log(`The temperature is ${TEMPERATURE} degrees Fahrenheit`);
+
